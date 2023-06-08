12f57f595caba28d6458324acaa7c2e6811dff11
Tom Stovall <stovak@gmail.com>
2023-06-08 16:03:00 -0700

    version works

--- main.go
+++ main.go
@@ -3,12 +3,29 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/stovak/go-terminus/cmd"
+import (
+	"fmt"
+	"os"
+	"os/exec"
 
-const (
-	Version = "4.0.0-alpha"
+	"github.com/stovak/go-terminus/cmd"
+)
+
+var (
+	Version    = "4.0.0-alpha"
+	CommitHash string
 )
 
 func main() {
 	cmd.Execute()
+}
+
+func init() {
+	commitHash, err := exec.Command("git", "rev-parse", "--short", "HEAD").Output()
+	if err != nil {
+		fmt.Println("Failed to retrieve Git commit hash:", err)
+		os.Exit(1)
+	}
+
+	CommitHash = string(commitHash)
 }
