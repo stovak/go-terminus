e3e88e1e955a9585d22249dbc30af2e6f5002477
Tom Stovall <stovak@gmail.com>
2023-06-08 13:23:24 -0700

    configuring bumpversion

---
+++ main.go
@@ -0,0 +1,14 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package main
+
+import "github.com/stovak/go-terminus/cmd"
+
+const (
+	Version = "4.0.0-alpha"
+)
+
+func main() {
+	cmd.Execute()
+}
